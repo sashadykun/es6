71794cf208cc2742c034a265d4af323970de050c
Sasha Dykun <sasha.dykun@parcelpending.com>
2019-10-23 16:40:22 -0700

    add react app

---
+++ README.md
@@ -0,0 +1,12 @@
+# To setup project using babel and webpack
+
+// need to be fill up
+
+- npm install react-dom react  babel-preset-react --save-dev
+
+- add   "babel": {
+    "presets": ["es2015", "react"]
+  },
+#	into package.json file
+
+- and add <div id="root"></div> into index.html
--- app/index.js
+++ app/index.js
@@ -1,26 +1,66 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 
-import { students, total } from './students';
-import { add } from './calculator';
-import multiply from './calculator';
-import Entity from './entity';
+const api_key = 'd6534755c5f3d0c59e7e125bf30e88d3'
 
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			city: 'Stanford',
+			description: ''
+		}
+	}
 
-class Hobbit extends Entity {
-	constructor(name, height){
-		super(name, height);
+	componentDidMount() {
+		this.grabWether(this.state.city);
+	}
+
+	grabWether(city) {
+		fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+		.then (res => res.json())
+		.then (json => {
+			this.setState({description: json.weather[0].description})
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Weather Report for {this.state.city}!</h1>
+				<h2>{this.state.description}</h2>
+			</div>
+		)
 	}
 }
 
-let Frodo = new Hobbit('Frodo Baggins', 4.5);
-
-console.log('frodo:', Frodo);
-Frodo.greet();
+ReactDom.render(<App/>, document.getElementById('root'));
 
 
-console.log(add(8,8));
-console.log(multiply(8,8));
 
-console.log(`imported students: ${students} total students is ${total}`);
+
+// import { students, total } from './students';
+// import { add } from './calculator';
+// import multiply from './calculator';
+// import Entity from './entity';
+
+
+// class Hobbit extends Entity {
+// 	constructor(name, height){
+// 		super(name, height);
+// 	}
+// }
+
+// let Frodo = new Hobbit('Frodo Baggins', 4.5);
+
+// console.log('frodo:', Frodo);
+// Frodo.greet();
+
+
+// console.log(add(8,8));
+// console.log(multiply(8,8));
+
+// console.log(`imported students: ${students} total students is ${total}`);
 // distruction assignment with arrays
 
 // let c =[100, 200];
@@ -60,23 +100,23 @@ console.log(`imported students: ${students} total students is ${total}`);
 
 // .map helper method
 
-let poins = [10, 20, 30];
+// let poins = [10, 20, 30];
 // we need to add one on each point
 
-let addOne = poins.map(number => number +1 );
+// let addOne = poins.map(number => number +1 );
  
-console.log(addOne);
+// console.log(addOne);
 
-let lengths = [3, 7, 5];
-let multiple = 8;
+// let lengths = [3, 7, 5];
+// let multiple = 8;
 
-const scale = (multiple) => {
-  return lengths.map(n => n*multiple);
-};
+// const scale = (multiple) => {
+//   return lengths.map(n => n*multiple);
+// };
 
 // const scale = multiple => lengths.map(n => n*multiple);
 
-console.log(scale(multiple));
+// console.log(scale(multiple));
 
 //--------------------------------------------------------------
 
@@ -86,19 +126,19 @@ console.log(scale(multiple));
 // 	return grade >=70;
 // }
 
-let scores = [80, 75, 45, 73, 56, 37, 89, 47];
+// let scores = [80, 75, 45, 73, 56, 37, 89, 47];
 
-let passing = scores.filter(element => element >=70);
+// let passing = scores.filter(element => element >=70);
 
-console.log('passing', passing);
+// console.log('passing', passing);
 // will return new array with values passes the callback function conditions;
 
-const averages = [6, 15.7, 9, 18, 22.1, 9.2, 4];
+// const averages = [6, 15.7, 9, 18, 22.1, 9.2, 4];
 
-const highlight = averages.filter(points => points > 15);
+// const highlight = averages.filter(points => points > 15);
 
 
-console.log('highlight', highlight);
+// console.log('highlight', highlight);
 
 //----------------------------------------------------------------
 
